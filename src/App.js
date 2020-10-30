9b58aa2a12a3eb0e4c6c25131f5b448630d43000
mukul <mukul@mukuls-MacBook-Pro.local>
2020-10-30 14:47:57 +0530

    spotify authentication added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from './Components/Login/Login'
+import React , { useEffect ,useState} from 'react'; 
+import { getTokenFromUrl } from './spotify';
 function App() {
+
+  const [token , setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = ''
+
+    const _token = hash.access_token;
+    if(_token){
+      setToken(_token)
+    }
+    console.log("Token ", token)
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      { token ?
+      (
+      <h1> Logged In </h1> 
+      ) :  
+     (
+     <Login/>
+     )
+    }
     </div>
   );
 }
